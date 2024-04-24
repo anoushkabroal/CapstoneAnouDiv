dca3160bf45c151c22f192908001938987ff2c58
= <=anoushkabroal@gmail.com>
2024-04-24 10:57:12 -0400

    4/23 Commit
    Running but issue with database connection

--- CapstoneAnouDiv/app/src/main/java/com/example/capstoneanoudiv/DatabaseManager.java
+++ CapstoneAnouDiv/app/src/main/java/com/example/capstoneanoudiv/DatabaseManager.java
@@ -2,7 +2,10 @@ package com.example.capstoneanoudiv;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLDataException;
+import java.sql.SQLException;
 
 public class DatabaseManager {
     private DatabaseHelper dbHelper;
@@ -14,10 +17,17 @@ public class DatabaseManager {
         context = c;
     }
 
+    public static Connection getConnection(String url) throws SQLException
+    {
+        //driverManager manages JDBC connection needed
+        return DriverManager.getConnection(url);
+    }
+
+
     public DatabaseManager open() throws SQLDataException
     {
         dbHelper = new DatabaseHelper(context);
-        database = dbHelper.getWritableDatabase();
+        database = dbHelper.getReadableDatabase();
         return this;
     }
 }
--- CapstoneAnouDiv/app/src/main/java/com/example/capstoneanoudiv/MainActivity.java
+++ CapstoneAnouDiv/app/src/main/java/com/example/capstoneanoudiv/MainActivity.java
@@ -2,15 +2,54 @@ package com.example.capstoneanoudiv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TextView;
 
-
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.myTextView);
+
+        DatabaseManager dbManager = new DatabaseManager(this);
+        // JDBC URL for SQLite
+        String url = "jdbc:sqlite:C:\\Users\\gaurav\\Desktop";
+
+        try (Connection connection = DatabaseManager.getConnection(url)) {
+            // SQL query (allow this to be changed by user)
+            String sqlQuery = "SELECT FoodProduct FROM food_ingredients_allergens WHERE Allergens = ?";
+
+            //PreparedStatement takes in a sqlQuery that can be changed
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+
+            // Set parameter value so the ? would be IT
+            preparedStatement.setString(1, "Dairy");
+
+            //do query and save the result
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Process the result set
+            while (resultSet.next()) {
+                String food = resultSet.getString("FoodProduct");
+                System.out.println("Food: " + food);
+                text.setText(food);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
